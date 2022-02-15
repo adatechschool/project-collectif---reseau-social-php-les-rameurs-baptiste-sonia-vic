7333b4ab1effea2fb749fc6f8fe726852f9d426d
Vico4 <victoire.stahl@gmail.com>
2022-02-15 15:03:10 +0100

    réglage des liens du header

--- header.php
+++ header.php
@@ -1,14 +1,18 @@
 <!doctype html>
    
    <header>
-            <?php $jeSuisConnecte = isset ($_SESSION["connected_id"]);?>
+            <?php $jeSuisConnecte = isset ($_SESSION["connected_id"]); ?>
+            
+            
             <a href='admin.php'><img src="resoc.jpg" alt="Logo de notre réseau social"/></a>
             <nav id="menu">
                 <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
+                <?php if($jeSuisConnecte) { 
+                $conUser = $_SESSION["connected_id"];
+                ?>
+                <a href="wall.php?user_id=<?php echo $conUser ?>">Mur</a>
                 <a href="tags.php?tag_id=1">Mots-clés</a>
-                <?php if($jeSuisConnecte) { ?>
-                <a href="feed.php?user_id=5">Flux</a>
+                <a href="feed.php?user_id=<?php echo $conUser ?>">Flux</a>
                 <a href="logout.php">Se déconnecter</a>
                 <?php }; ?> 
             </nav>
@@ -20,9 +24,9 @@
             } else { ?>
                 <a href="#">▾ Profil</a>
                 <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                    <li><a href="settings.php?user_id=<?php echo $conUser ?>">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=<?php echo $conUser ?>">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=<?php echo $conUser ?>">Mes abonnements</a></li>
                 </ul>
                   <?php } ?>            
             </nav>
